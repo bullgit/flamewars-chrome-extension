e14046db8d6bcae00f61e9ab15de12cb37c979fc
Michael Kühnel <m.kuehnel@micromata.de>
2015-12-02 13:13:54 +0100

    Version 0.0.2

---
+++ app/scripts/contentscript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+
+while (treeWalker.nextNode()) {
+  var node = treeWalker.currentNode;
+  node.nodeValue = node.nodeValue.replace(/(F|f)ramework/g, '$1lamewar');
+}
+//# sourceMappingURL=contentscript.js.map
